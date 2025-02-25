3a7706e878d3784dbea1ffc87cd9294d3ca211fd
dedews07 <dewisulisdewisulis0@gmail.com>
2025-02-25 13:45:32 +0700

    "percobaan routing"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+
+ Route::get('/world', function () {
+    return 'World';
+ });
+ 
+ Route::get('/', function () {
+    return 'Selamat Datang!';
+});
+
+Route::get('/about', function () {
+    return 'NIM  : 2341760126 <br>
+            Nama : Dewi Sulistiyowati!';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
+    });
+    
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/articles/{id}', function ($id) {
+        return "Halaman Artikel dengan ID $id";
+    });
+    
+// Route::get('/user/{name?}', function ($name='Dewi') {
+//         return "<h1>Nama saya $name </h1>";
+//         });
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +47,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
